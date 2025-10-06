39768795f795403f50e511fc28f92effccffac8a
IBN-Ibrahim26 <silverbuur99@gmail.com>
2025-10-06 17:06:09 +0000

    liste des depenses

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'config/env.php';
-try
-{
-    $mysqlClient=new PDO("mysql:host=localhost;dbname=gestion_depenses;charset=utf8", "root","" );
-}
-catch (Exception $e)
-{
+try {
+    $mysqlClient = new PDO(
+        "mysql:host=localhost;port=3307;dbname=gestion_depenses;charset=utf8",
+        "root",
+        ""
+    );
+} catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
-
 ?>
